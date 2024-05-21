4340519239cacbe3ce54f1cbe719a390524c7171
Marilia Morais <marilia.morais@marilia.morais>
2024-05-21 13:54:44 -0300

    feat: Add project to calculate simple statistics functions

--- src/small_projects/stats.clj
+++ src/small_projects/stats.clj
@@ -1,1 +1,28 @@
 (ns small-projects.stats)
+
+
+(defn average
+  [data]
+  (cond
+    (= (count data) 0) nil
+    (not (= data 0)) (double (/ (reduce + data) (count data)))))
+
+(defn mode
+  [data]
+  (let [frequencies (reduce #(update %1 %2 (fnil inc 0)) {} data)
+        max-frequency (apply max (vals frequencies))
+        mode-result (filter #(= max-frequency (frequencies %)) (keys frequencies))]
+    (first mode-result)))
+
+(defn median-for-even-collection
+  [data
+   middle-index]
+  (/ (+ (data middle-index) (data (dec middle-index))) 2))
+
+(defn median
+  [data]
+  (let [len (count data)
+        middle-index (int (inc (/ len 2)))]
+    (cond
+      (even? len) (median-for-even-collection data middle-index)
+      (odd? len) (data middle-index))))
